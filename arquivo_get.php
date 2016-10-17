dbead12bb4beced4dc41f107d722d29eceb3e12d
Sistema Audio Cristão <manoel@arquivocristao.com.br>
2016-10-16 22:52:26 -0200

    Create arquivo_get.php

---
+++ arquivo_get.php
@@ -0,0 +1,21 @@
+maria
+<?php
+
+$nome = $_GET["txtnome"];
+$idade = $_GET["txtidade"]; 
+$uma = $_GET["uma"]; 
+echo " ".$uma." ".$nome."  @ ".$idade." - ";
+
+?>
+
+paulo
+<?php
+
+$nome = $_GET["txtnome"];
+$idade = $_GET["txtidade"]; 
+$uma = $_GET["uma"]; 
+echo " ".$uma." ".$nome."  @ ".$idade." - ";
+
+?>
+
+
